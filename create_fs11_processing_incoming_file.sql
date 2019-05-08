e53abc1791e2271400af8efdc1cf31086307c570
Ekesha <ekaterina.kalachina@aiesec.net>
2019-05-09 03:22:10 +0700

    ~

--- create_fs11_processing_incoming_file.sql
+++ create_fs11_processing_incoming_file.sql
@@ -1,15 +1,16 @@
--- +for all i in 1 ?, +exceptions with rollback and sql%isopen & close, +new collection for file-status extended ass array
+-- +for all i in 1 ? (insert?), +exceptions with rollback and sql%isopen & close, +new collection for file-status extended ass array
 -- id_record, status (new, processed, rejected), error_message
 -- + 2 variables for check header an check trailer
 -- excep when p after r. excep when new card, merch, mcc (?). if file is incorrect - no new card/merch/mcc!
 -- + inserts
 
 create or replace package fs11_processing_incoming_file as
-    procedure fs11_proc_file(p_file_name varchar2, p_file_string varchar2); -- point of entry. public(cause the declare in the specification)
+    procedure fs11_proc_file(p_file_name varchar2, p_file_string varchar2); -- point of entry. public(cause declaring in the specification)
 end fs11_processing_incoming_file;
 
--- two variables for header and trailer isnt public
-    create or replace package body fs11_processing_incoming_file as
+-- two variables for header and trailer isnt public ?
+
+create or replace package body fs11_processing_incoming_file as
 
     type fields_tab is table of varchar2(2000);
     array fields_tab := fields_tab();
@@ -39,44 +40,44 @@ end fs11_processing_incoming_file;
         null;
     end;
 
-    procedure fs11_parse as
+    procedure fs11_parse_record as
         delimiter_pos  number;
         record_end_pos number;
-        c              integer := 0;
+        record_field_number integer := 0;
     begin
-        select INSTR(file_string, chr(10), file_pos) into record_end_pos from DUAL;
+        record_end_pos := INSTR(file_string, chr(10), file_pos);
         array.delete;
         -- if it is last record then we don't have delimiter after last field
         if record_end_pos = file_length then
             loop
-                select INSTR(file_string, ';', file_pos) into delimiter_pos from DUAL;
-                c := c + 1;
+                delimiter_pos := INSTR(file_string, ';', file_pos);
+                record_field_number := record_field_number + 1;
                 array.extend;
-                -- resolve upper comment probem here
+                -- resolve upper comment problem here
                 if delimiter_pos = 0 then
-                    select SUBSTR(file_string, file_pos, file_length - file_pos) into array(c) from DUAL;
+                    array(record_field_number) := SUBSTR(file_string, file_pos, file_length - file_pos);
                     file_pos := file_length;
                     exit;
                 end if;
                 if delimiter_pos < record_end_pos then
-                    select SUBSTR(file_string, file_pos, delimiter_pos - file_pos) into array(c) from DUAL;
+                    array(record_field_number) := SUBSTR(file_string, file_pos, delimiter_pos - file_pos);
                     file_pos := delimiter_pos + 1;
                 else
-                    select SUBSTR(file_string, file_pos, delimiter_pos - file_pos - 2) into array(c) from DUAL;
+                    array(record_field_number) := SUBSTR(file_string, file_pos, delimiter_pos - file_pos - 2);
                     file_pos := delimiter_pos - 1;
                     exit;
                 end if;
             end loop;
         else
             loop
-                select INSTR(file_string, ';', file_pos) into delimiter_pos from DUAL;
-                c := c + 1;
+                delimiter_pos := INSTR(file_string, ';', file_pos);
+                record_field_number := record_field_number + 1;
                 array.extend;
                 if delimiter_pos < record_end_pos then
-                    select SUBSTR(file_string, file_pos, delimiter_pos - file_pos) into array(c) from DUAL;
+                    array(record_field_number) := SUBSTR(file_string, file_pos, delimiter_pos - file_pos);
                     file_pos := delimiter_pos + 1;
                 else
-                    select SUBSTR(file_string, file_pos, delimiter_pos - file_pos - 2) into array(c) from DUAL;
+                    array(record_field_number) := SUBSTR(file_string, file_pos, delimiter_pos - file_pos - 2);
                     file_pos := delimiter_pos - 1;
                     exit;
                 end if;
@@ -104,11 +105,11 @@ end fs11_processing_incoming_file;
         refund_count := 0;
         file_pos := 1;
 
-        select LENGTH(file_string) into file_length from DUAL;
+        file_length := LENGTH(file_string);
         dbms_output.put_line('File length: ' || file_length);
 
         loop
-            fs11_parse;
+            fs11_parse_record;
             case (array(1))
                 when 'H' then
                     -- can process header here
