dd38eedaac1ec3364f7a0a05f0465c0ba1dfa569
Nikolay Voronkov <voronkov.nikolay@gmail.com>
2019-05-19 03:33:34 +0700

    add file generator

--- test_call.sql
+++ test_call.sql
@@ -1,12 +1,37 @@
 declare
-    file_string varchar2(32767) := 'H;11;20191103003100' ||
-                                   chr(10) ||
-                                   'P;1234567891abcdefghij1234567890abcdefghij;3;20191103003100;0;0;1234;comment' ||
-                                   chr(10) ||
-                                   'R;1234567892abcdefghij1234567890abcdefghij;5;20191103003100;0;0;1d;' ||
-                                   chr(10) ||
-                                   'T;1;1' ||
-                                   chr(10);
+    function generate_file_content(transaction_count number, refunds_count number) return varchar2 as
+        content varchar2(32767);
+        transaction varchar2(32767);
+    begin
+        content := 'H;' || DBMS_RANDOM.STRING('x', 12) || ';';
+        content := content || to_char(sysdate, 'yyyymmddhh24miss') || chr(10);
+        for void in 1 .. transaction_count - refunds_count
+        loop
+            transaction := 'P;' || DBMS_RANDOM.STRING('x', 40) || ';';
+            transaction := transaction || DBMS_RANDOM.STRING('x', 12) || ';';
+            transaction := transaction || to_char(sysdate, 'yyyymmddhh24miss') || ';';
+            transaction := transaction || trunc(DBMS_RANDOM.VALUE * 999999999999) || ';';
+            transaction := transaction || DBMS_RANDOM.STRING('x', 30) || ';';
+            transaction := transaction || trunc(DBMS_RANDOM.VALUE * 8999 + 1000)  || ';';
+            transaction := transaction || ';' || chr(10);
+            content := content || transaction;
+        end loop;
+        for void in 1 .. refunds_count
+        loop
+            transaction := 'R;' || DBMS_RANDOM.STRING('x', 40) || ';';
+            transaction := transaction || DBMS_RANDOM.STRING('x', 12) || ';';
+            transaction := transaction || to_char(sysdate, 'yyyymmddhh24miss') || ';';
+            transaction := transaction || trunc(DBMS_RANDOM.VALUE * 999999999999) || ';';
+            transaction := transaction || DBMS_RANDOM.STRING('x', 30) || ';';
+            transaction := transaction || DBMS_RANDOM.STRING('x', 12) ||  ';';
+            transaction := transaction || ';' || chr(10);
+            content := content || transaction;
+        end loop;
+        content := content || 'T;' || to_char(transaction_count - refunds_count) || ';' || to_char(refunds_count) || chr(10);
+        return content;
+
+    end;
+
 begin
-    FS11_PROCESSING_INCOMING_FILE.FS11_PROC_FILE('test.csv', file_string);
+    FS11_PROCESSING_INCOMING_FILE.FS11_PROC_FILE('stub', generate_file_content(100, 10));
 end;
