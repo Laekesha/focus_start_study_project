2d479ee8800e0b1bc4b0bc3bea52ad1ad952325d
Nikolay Voronkov <voronkov.nikolay@gmail.com>
2019-05-22 23:29:35 +0700

    trunc proc

--- test_call.sql
+++ test_call.sql
@@ -1,4 +1,21 @@
 declare
+    procedure print(p_message varchar2) as
+    begin
+        dbms_output.put_line(p_message);
+    end;
+
+    function random_string(lenght number) return varchar2
+        as 
+        begin 
+            return dbms_random.string('x', lenght);
+        end;
+
+     function random_number(upper_bound number) return number
+        as
+        begin
+            return trunc(dbms_random.value * upper_bound);
+        end;
+    
     function generate_file_content(file_id varchar2, transaction_count number, refunds_count number) return clob as
         content clob;
         transaction varchar2(32767);
@@ -7,24 +24,24 @@ declare
         content := content || to_char(sysdate, 'yyyymmddhh24miss') || chr(10);
         for void in 1 .. transaction_count - refunds_count
         loop
-            transaction := 'P;' || DBMS_RANDOM.STRING('x', 40) || ';';
-            transaction := transaction || DBMS_RANDOM.STRING('x', 12) || ';';
+            transaction := 'P;' || random_string(40) || ';';
+            transaction := transaction || random_string(12) || ';';
             transaction := transaction || to_char(sysdate, 'yyyymmddhh24miss') || ';';
-            transaction := transaction || trunc(DBMS_RANDOM.VALUE * 9999999999) || ';';
-            transaction := transaction || DBMS_RANDOM.STRING('x', 30) || ';';
-            transaction := transaction || trunc(DBMS_RANDOM.VALUE * 8999 + 1000)  || ';';
-            transaction := transaction || 'comment' /* comment here */ || chr(10);
+            transaction := transaction || random_number(9999999999) || ';';
+            transaction := transaction || random_string(30) || ';';
+            transaction := transaction || random_number(8999) + 1000  || ';';
+            transaction := transaction || random_string(random_number(2000)) || chr(10);
             content := content || transaction;
         end loop;
         for void in 1 .. refunds_count
         loop
-            transaction := 'R;' || DBMS_RANDOM.STRING('x', 40) || ';';
-            transaction := transaction || DBMS_RANDOM.STRING('x', 12) || ';';
+            transaction := 'R;' || random_string(40) || ';';
+            transaction := transaction || random_string(12) || ';';
             transaction := transaction || to_char(sysdate, 'yyyymmddhh24miss') || ';';
-            transaction := transaction || trunc(DBMS_RANDOM.VALUE * 9999999999) || ';';
-            transaction := transaction || DBMS_RANDOM.STRING('x', 30) || ';';
-            transaction := transaction || DBMS_RANDOM.STRING('x', 12) ||  ';';
-            transaction := transaction || 'comment' /* comment here */ || chr(10);
+            transaction := transaction || random_number(9999999999) || ';';
+            transaction := transaction || random_string(30) || ';';
+            transaction := transaction || random_string(12) ||  ';';
+            transaction := transaction || random_string(random_number(2000))|| chr(10);
             content := content || transaction;
         end loop;
         content := content || 'T;' || to_char(transaction_count - refunds_count) || ';' || to_char(refunds_count) || chr(10);
@@ -32,25 +49,41 @@ declare
 
     end;
 
+    procedure trunc_file_tables as
+    begin
+        execute immediate 'alter table FS11_FILE_CONTENT disable constraint FK_FILE_CONTENT_TO_FILE_RECORDS';
+        execute immediate 'truncate table FS11_FILE_RECORDS';
+        execute immediate 'alter table FS11_FILE_CONTENT enable constraint FK_FILE_CONTENT_TO_FILE_RECORDS';
+        execute immediate 'truncate table FS11_FILE_CONTENT';
+        print('FS11_FILE_CONTENT and FS11_FILE_RECORDS truncated');
+    end;
+
     procedure add_file as
         file_content clob;
         file_id     varchar2(12);
     begin
-        file_id := DBMS_RANDOM.STRING('x', 12);
+        file_id := random_string(12);
         file_content := generate_file_content(file_id, 10000, 0);
         insert into FS11_FILE_RECORDS values (file_id, 'file', sysdate, 'incoming', 'new', null);
         insert into FS11_FILE_CONTENT values (file_id, file_content);
         commit;
     end;
 
+    procedure proc_file_table as
+    begin
+        FOR ids IN
+            (select file_id
+             FROM FS11_FILE_CONTENT
+                      natural join FS11_FILE_RECORDS
+             where FILE_STATUS = 'new'
+               and FILE_TYPE = 'incoming')
+            loop
+                print('Process: ' || ids.FILE_ID);
+                FS11_PROCESSING_INCOMING_FILE.FS11_PROC_FILE(ids.FILE_ID);
+            end loop;
+    end;
+
 begin
 --     add_file;
-    FOR ids IN
-        (select file_id FROM FS11_FILE_CONTENT
-         natural join FS11_FILE_RECORDS where FILE_STATUS = 'new' and FILE_TYPE = 'incoming')
-        loop
-            DBMS_OUTPUT.put_line('Process: ' || ids.FILE_ID);
-            FS11_PROCESSING_INCOMING_FILE.FS11_PROC_FILE(ids.FILE_ID);
-        end loop;
---
+    trunc_file_tables;
 end;
